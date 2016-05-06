260656e44f433e0e96603fda8cc72a90eecebc09
Andrii Peliak <vseprav@gmail.com>
2016-05-06 20:13:59 +0300

    set up spring app

---
+++ src/main/java/ua/com/vseprav/PocketDictionaryApplication.java
@@ -0,0 +1,12 @@
+package ua.com.vseprav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketDictionaryApplication.class, args);
+	}
+}
