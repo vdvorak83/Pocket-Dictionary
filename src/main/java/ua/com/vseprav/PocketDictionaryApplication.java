fce2a583bde797f2c4980393685f9b58e022fa6a
Andrii Peliak <vseprav@gmail.com>
2016-05-11 16:13:39 +0300

    added setup for tomcat

--- src/main/java/ua/com/vseprav/PocketDictionaryApplication.java
+++ src/main/java/ua/com/vseprav/PocketDictionaryApplication.java
@@ -2,11 +2,18 @@ package ua.com.vseprav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PocketDictionaryApplication {
+public class PocketDictionaryApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocketDictionaryApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PocketDictionaryApplication.class);
 	}
 }
--- src/main/java/ua/com/vseprav/web/controllers/MainPageController.java
+++ src/main/java/ua/com/vseprav/web/controllers/MainPageController.java
@@ -1,14 +1,15 @@
 package ua.com.vseprav.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainPageController {
 
     @RequestMapping(value = "/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("appName","Pocket Dictionary Application");
         return "main_page";
     }
 
