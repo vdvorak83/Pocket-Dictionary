74a1084a702d51e0a1ff710d05a0e5558f4e66d8
Andrii Peliak <vseprav@gmail.com>
2016-05-16 17:53:36 +0300

    added Spring Security

--- src/main/java/ua/com/vseprav/web/controllers/MainPageController.java
+++ src/main/java/ua/com/vseprav/web/controllers/MainPageController.java
@@ -3,14 +3,15 @@ package ua.com.vseprav.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainPageController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model){
         model.addAttribute("appName","Pocket Dictionary Application");
-        return "main_page";
+        return "views/main_page";
     }
 
 }
