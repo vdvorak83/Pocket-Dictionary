c6e530ce24ad1ff32770c4b119523490015c40cc
Andrii Peliak <vseprav@gmail.com>
2016-05-20 20:06:55 +0300

    fix logout, implement global exception handler

--- src/main/java/ua/com/vseprav/config/SecurityConfig.java
+++ src/main/java/ua/com/vseprav/config/SecurityConfig.java
@@ -22,8 +22,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
-                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/resources/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
