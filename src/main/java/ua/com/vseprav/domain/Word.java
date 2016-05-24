287df5ff4fb0fa6e57b7f27369894b0b1e074654
Andrii Peliak <vseprav@gmail.com>
2016-05-24 14:49:03 +0300

    add spring data dependency

---
+++ src/main/java/ua/com/vseprav/domain/Word.java
@@ -0,0 +1,31 @@
+package ua.com.vseprav.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Word {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String text;
+}
